abf09631354cd5a257daf79500ea5f8867f78ae3
paulballesty <paulballesty@a11896a5-102c-a516-f8db-4dec3e81e806>
2010-09-02 22:47:34 +0000

    fifo con serverloop

--- trunk/tests/ipc_fifo_server.c
+++ trunk/tests/ipc_fifo_server.c
@@ -1,45 +1,7 @@
 #include "../include/ipc_fifo.h"
 
-/*
 int main(){
-	
-	initFifos(1);
-	char auxname[50];
-	message_t inmsg;
-
-
-	ipcdata_t auxData = fifoIPCData(0);
-
-	strcpy(auxname, auxData->fifodata.fifonamer);
-	strcpy(auxData->fifodata.fifonamer, auxData->fifodata.fifonamew);
-	strcpy(auxData->fifodata.fifonamew, auxname);
-
-	ipc_t server = fifoConnect(auxData); 
-	
-	strcpy(server->ipcdata->fifodata.fifonamer, "/tmp/fifo_c_w_0");
-	strcpy(server->ipcdata->fifodata.fifonamew, "/tmp/fifo_c_r_0");
-
-	char * msg1 = calloc(sizeof(char), 10);
-	strcpy(msg1, "s.msg");
-
-	char * word = "Soy el server y le mando este mensaje al server porque me la banco";
-
-	char a;
-
-	fifoConnect(server->ipcdata);
-
-	while(1){
-		qput(server->outbox, mnew(0,0,strlen(word)+1, word));
-		inmsg = qget(server->inbox);
-		if(inmsg != NULL){
-			printf("Server recibio: %s\n", inmsg->data);
-		}
-		sleep(2);	
-	}	
-}
-*/
-
-int main(){
+	/*	
 	initFifos(1);
 	ipc_t server;
 	char auxname[50];
@@ -58,6 +20,18 @@ int main(){
 		qput(server->outbox, mnew(0,0,strlen(word) + 1, word));
 		if((inmsg = qget(server->inbox)) != NULL){
 			printf("server recibio: %s\n", inmsg->data);
+		}
+		sleep(2);
+	}
+	*/
+
+	message_t incomingMsg;
+
+	ipc_t server = (ipc_t) fifoServe(NULL, 2);
+	server->stop = 0;
+	while(1){
+		if((incomingMsg = qget(server->inbox)) != NULL){
+			printf("Server recibio: %s\n", incomingMsg->data);
 		}
 	}
 }
