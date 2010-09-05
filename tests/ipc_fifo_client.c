60f5acd56cbcd555ec5b04b2924ea0e1a677b46a
paulballesty <paulballesty@a11896a5-102c-a516-f8db-4dec3e81e806>
2010-09-05 05:22:42 +0000

    fifos testeado para 10 clientes y 1000 mensajes

--- tests/ipc_fifo_client.c
+++ tests/ipc_fifo_client.c
@@ -12,16 +12,19 @@ int main(int argc, char * argv[]){
 
 	client = fifoConnect(fifoIPCData(antid));
 	printf("Cliente iniciado id: %d\n", antid);
-	message_t inmsg;
+	message_t inmsg, aux;
 	char word[50];
 	sprintf(word, "Mensaje del cliente con id: %d", antid);
 	
 	while(1){
-		printf("mando mensaje\n");
-		qput(client->outbox, mnew(antid,-1,strlen(word) + 1, word));
+		printf("Mando mensaje\n");
+		qput(client->outbox, (aux = mnew(antid,0,strlen(word) + 1, word)));
+		mdel(aux);
 		if((inmsg = qget(client->inbox)) != NULL){
 			printf("Client recibio: %s\n", inmsg->data);
+			mdel(inmsg);
 		}
-		sleep(3);
+		sleep(1);
 	}
+	fifoDisconnect(client);
 }
