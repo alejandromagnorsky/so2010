e9b34b854b72dc65c94900dbee29dcaccd0137c5
jimenabpose <jimenabpose@a11896a5-102c-a516-f8db-4dec3e81e806>
2010-09-12 23:58:24 +0000

    puntos y turnos agregados

--- trunk/tests/ioTest.bash
+++ trunk/tests/ioTest.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -lncurses -lm ioTest.c ../include/common.h ../include/io.h ../src/io.c -lncurses -o ioTest
+gcc -lncurses -lm ioTest.c ../include/common.h ../include/io.h ../include/common.h ../src/io.c -lncurses -o ioTest
