11dd357fe6572ce8185f51ade94b1103011a92c7
paulballesty <paulballesty@a11896a5-102c-a516-f8db-4dec3e81e806>
2010-09-23 18:49:13 +0000

    Paginacion, si anda, me muero

--- trunk/tp2/include/mem.h
+++ trunk/tp2/include/mem.h
@@ -3,17 +3,39 @@
 
 /* Pages of 4KB */
 #define PAGESIZE 0x1000
+#define NENTRIES 1024
 
-/* Number of pages for 4GB*/
-#define NUMPAGES 0x400
+/* The firsts 4MB are for the kernel, the page directory and the kernel table.*/
+/* The last page of the first 4MB will be the page directory */
+#define PD_DIR_POS (unsigned int*)0x3FF000
+/* The predecessor page will be the Kernel Table, to do Indentity Mapping*/
+#define KERNEL_TABLE_POS (unsigned int*) 0x3FE000 
+
+typedef (unsigned int*) pdir_t;
 
 #define SETBIT(X, P, V) (V ? ((1 << (P)) | (X)) : (~(1 << (P)) & X))
+#define SET_PRESENT(X,V) SETBIT(X,0,V)
+#define SET_RDWR(X,V) SETBIT(X,1,V)
+#define SET_SUPERVISOR(X,V) SETBIT(X,2,V)
 
 typedef int pdentry_t;
+
+/* Creates the page directory with the firsts 4MB using Identity Mapping.
+ * Fill the Page directory with the Kernel Table.
+ * And sets the CR3 registry.
+ * */
+pdir_t _pd_create(void);
+
+pdir_t _pd_createEmptyDirectoryPage();
+void _setCR3(pdir_t directory_pos);
+pdir_t _kernelTable();
+void _setCR3(pdir_t page_directory);
+void _enablePaging(pdir_t page_directory);
+
 
 /* The following functions take a pointer to a Page Directory Entry, and fill
    the structure with the given information */
 pdentry_t   _pd_createEntry(void* address);
 void        _pd_togglePresent(pdentry_t*);
-void		_enablePaging();
+
 #endif
--- trunk/tp2/src/mem.c
+++ trunk/tp2/src/mem.c
@@ -1,18 +1,20 @@
-/*
-** Task functions file
-*/
-
 #include "../include/mem.h"
 
 struct {
     
     pdentry_t (*createEntry) (void*);
     void (*togglePresent) (pdentry_t*);
-    void (*_enablePaging) ();
     
 } PageDirectory = { _pd_createEntry,
-                    _pd_togglePresent,
-                    _enablePaging };
+                    _pd_togglePresent
+				};
+
+struct {
+    
+    void (*start) (pdentry_t*);
+    
+} Paging = { _startPaging};
+
 
 
 pdentry_t _pd_createEntry(void* address) {
@@ -25,7 +27,45 @@ void _pd_togglePresent(pdentry_t* entry) {
     return;
 }
 
-void _enablePaging()
-{
-	_setPG ( _read_cr0() | 0x80000000 );
+
+void _startPaging(){
+	pdir_t page_directory = _setCR3(_pd_createEmptyDirectoryPage());
+	page_directory[0] = _kernelTable();
+	SET_PRESENT(page_directory[0]);
+	_setCR3(pdir_t page_directory);
+	_enablePaging(pdir_t page_directory);
+}
+
+pdir_t _pd_createEmptyDirectoryPage(){
+	pdir_t page_directory = PD_DIR_POS;
+	int i = 0;
+	for(i = 0; i < NENTRIES; i++)
+	{
+	    page_directory[i] = 0; 
+	    SET_PRESENT(page_directory[i], 0);
+	    SET_RDWR(page_directory[i]);
+	    SET_SUPERVISOR(page_directory[i]);
+	}
+}
+
+pdir_t _kernelTable(){
+	pdir_t kernel_table = KERNEL_TABLE_POS;
+	int i = 0;
+	for(i = 0; i < NENTRIES; i++){
+		page_directory[i] = i * PAGESIZE;
+	    SET_PRESENT(page_directory[i], 1);
+	    SET_RDWR(page_directory[i], 1);
+	    SET_SUPERVISOR(page_directory[i], 1);
+	}
+}
+
+void _setCR3(pdir_t page_directory){
+	asm volatile("mov %0, %%cr3":: "b"(page_directory));
+}
+
+void _enablePaging(pdir_t page_directory){
+	unsigned int cr0;
+	asm volatile("mov %%cr0, %0": "=b"(cr0));
+	cr0 |= 0x80000000;
+	asm volatile("mov %0, %%cr0":: "b"(cr0));
 }
